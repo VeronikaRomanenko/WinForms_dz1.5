a626c46c5beb19b5c3831a5c67aa51d1abe25e96
VeronikaRomanenko <Veronika.Romanenko333@gmail.com>
2019-04-19 15:01:02 +0300

    Commit1

---
+++ WinForms_dz1.5/WinForms_dz1.5/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinForms_dz1._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStatic = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStatic
+            // 
+            this.btnStatic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStatic.Location = new System.Drawing.Point(324, 169);
+            this.btnStatic.Name = "btnStatic";
+            this.btnStatic.Size = new System.Drawing.Size(126, 79);
+            this.btnStatic.TabIndex = 0;
+            this.btnStatic.Text = "НАЖМИ МЕНЯ";
+            this.btnStatic.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStatic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStatic;
+    }
+}
+
---
+++ WinForms_dz1.5/WinForms_dz1.5/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_dz1._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (((btnStatic.Location.X - e.X) > -75 && (btnStatic.Location.X - e.X) < 75)
+                || (btnStatic.Location.Y - e.Y) > -75 && (btnStatic.Location.Y - e.Y) < 75)
+            {
+                Random rnd = new Random();
+                btnStatic.Location = new Point(rnd.Next(this.Size.Width - 100), rnd.Next(this.Size.Height - 100));
+            }
+        }
+    }
+}
